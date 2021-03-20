d3df4347e3eb45731e3d759e0baefbe991ba08d8
Rahul Badgujar <44890430+imRB7here@users.noreply.github.com>
2021-03-20 12:04:09 +0530

    configured firebase for project

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(HomePage());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -48,68 +51,83 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Mytodos"),
+      appBar: AppBar(
+        title: Text("Mytodos"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                title: Text("Add Todos"),
+                content: TextField(
+                  onChanged: (String value) {
+                    input = value;
+                  },
+                ),
+                actions: <Widget>[
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Add")),
+                ],
+              );
+            },
+          );
+          await addTodo(input);
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)),
-                    title: Text("Add Todos"),
-                    content: TextField(
-                      onChanged: (String value) {
-                        input = value;
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection("mytodo").snapshots(),
+        builder: (context, snapshots) {
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: snapshots.data.docs.length,
+            itemBuilder: (context, index) {
+              final docMap = snapshots.data.docs[index].data();
+              return Dismissible(
+                key: Key(index.toString()),
+                child: Card(
+                  elevation: 4,
+                  margin: EdgeInsets.all(8),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: ListTile(
+                    title: Text("${docMap["title"]}"),
+                    //subtitle: Text("${docMap['description']}"),
+                    trailing: IconButton(
+                      icon: Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                      onPressed: () {
+                        setState(
+                          () {
+                            todos.removeAt(index);
+                          },
+                        );
                       },
                     ),
-                    actions: <Widget>[
-                      TextButton(
-                          onPressed: () {
-                            createTodos();
-                            Navigator.of(context).pop();
-                          },
-                          child: Text("Add")),
-                    ],
-                  );
-                });
-          },
-          child: Icon(
-            Icons.add,
-            color: Colors.white,
-          ),
-        ),
-        body: StreamBuilder(
-            stream: FirebaseFirestore.instance.collection("MyTodos").snapshots(),
-            builder: (context, snapshots) {
-              return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: snapshots.data.document.length,
-                  itemBuilder: (context, index) {
-                    DocumentSnapshot documentSnapshot =
-                        snapshots.data.documents[index];
-                    return Dismissible(
-                        key: Key(index.toString()),
-                        child: Card(
-                            elevation: 4,
-                            margin: EdgeInsets.all(8),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8)),
-                            child: ListTile(
-                                title: Text(documentsnapshots["todotitle"]),
-                                trailing: IconButton(
-                                    icon: Icon(
-                                      Icons.delete,
-                                      color: Colors.red,
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        todos.removeAt(index);
-                                      });
-                                    }))));
-                  });
-            }));
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Future<void> addTodo(String input) async {
+    final collRef = FirebaseFirestore.instance.collection("mytodo");
+    await collRef.add({"title": input});
   }
 }
